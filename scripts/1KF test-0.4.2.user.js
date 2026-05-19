72e408ef621bda03a3f4b639fd0186d81c101959
sillyshark123 <isaac.lau99@gmail.com>
2026-05-19 15:04:10 -0400

    Update 1KF-test-0.4.2.user.js

--- scripts/1KF test-0.4.2.user.js
+++ scripts/1KF test-0.4.2.user.js
@@ -52,6 +52,7 @@ window.OneKF = class OneKF {
     static rowRotations = [2,3,0,1];
 
     // keycodes. change only if your keyboard layout isn't qwerty
+    // on Firefox the semicolon key is 59 instead of 186
     static keyCodes = [[49, 50, 51, 52, 53, 54, 55, 56, 57, 48],
                        [81, 87, 69, 82, 84, 89, 85, 73, 79, 80],
                        [65, 83, 68, 70, 71, 72, 74, 75, 76, 186],
